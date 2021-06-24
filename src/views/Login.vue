d170f5831a7ea21071ecdfdf0ae2bb94860026f9
tommyjqliu <tommyjqliu@tencent.com>
2021-06-24 22:15:25 +0800

    增加登录

---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <el-input v-model="username" placeholder="请输入账号"></el-input>
+    <el-input v-model="password" placeholder="请输入密码" show-password></el-input>
+    <el-row>
+      <el-button type="primary" @click="login">登录</el-button>
+      <el-button @click="reset">重置</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    reset() {
+      this.username = '';
+      this.password = '';
+    },
+    login() {
+      fetch('api/signin',{
+        method: 'POST',
+        body: JSON.stringify({
+          username: this.username,
+          password: this.password,
+        })
+      }).then((res)=>{
+        console.log(res);
+      })
+    }
+  }
+}
+</script>
